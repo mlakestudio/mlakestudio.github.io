56662a861c95495eacf2c94688243993347fc764
e3-180 <mphouse16@naver.com>
2025-02-26 09:11:38 +0900

    update

---
+++ get-holiday-with-year.bat
@@ -0,0 +1,12 @@
+holiday_manager.exe %1 01
+holiday_manager.exe %1 02
+holiday_manager.exe %1 03
+holiday_manager.exe %1 04
+holiday_manager.exe %1 05
+holiday_manager.exe %1 06
+holiday_manager.exe %1 07
+holiday_manager.exe %1 08
+holiday_manager.exe %1 09
+holiday_manager.exe %1 10
+holiday_manager.exe %1 11
+holiday_manager.exe %1 12
